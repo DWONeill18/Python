5a12a6821d1543a3ff73f62561309f6d02ac0631
DWONeill18 <d.w.oneill.18@gmail.com>
2020-10-15 17:05:26 +0100

    Created boxplots for goal data

--- Seaborn/projects/world_cup_data/world_cup_matches.py
+++ Seaborn/projects/world_cup_data/world_cup_matches.py
@@ -6,11 +6,11 @@ import seaborn as sns
 
 # Load data into a dataframe
 df = pd.read_csv("WorldCupMatches.csv")
-print(df.head())
+#print(df.head())
 
 # Create new column of Total Goals
 df['Total Goals'] = df['Home Team Goals'] + df['Away Team Goals']
-print(df.head())
+#print(df.head())
 
 sns.set_style("whitegrid")
 sns.set_context("poster")
@@ -22,5 +22,23 @@ ax = sns.barplot(
   y="Total Goals"
 )
 ax.set_title("A Comparison of Total Goals scored in World Cups")
+plt.show()
+
+
+# Box plots
+
+df_goals = pd.read_csv("goals.csv")
+#print(df_goals.head())
+
+sns.set_context("notebook", font_scale=1.25)
+f, ax2 = plt.subplots(figsize=(12 ,7))
+
+ax2 = sns.boxplot(
+  data=df_goals,
+  x="year",
+  y="goals",
+  palette="Spectral"
+)
+ax2.set_title("Box plots to show distribution of Goals for each World Cup")
 
 plt.show()
